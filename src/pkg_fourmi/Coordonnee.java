d32c79d457ea42c8f82a6825481ad760a0e6f091
SaintCrow <valehue@hotmail.fr>
2018-04-10 11:53:37 +0200

    maj case coord Valentin

--- src/pkg_fourmi/Coordonnee.java
+++ src/pkg_fourmi/Coordonnee.java
@@ -1,5 +1,66 @@
 package pkg_fourmi;
 
 public class Coordonnee {
+	private int x;
+	private int y;
+
+	/**
+	 * constructeur
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public Coordonnee(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * 
+	 * @param c
+	 *            other coordinate
+	 * @return distance entre les l'objet et c, pas de déplacement sur les
+	 *         diagonales
+	 */
+	public int distance(Coordonnee c) {
+		return Math.abs(this.x - c.getX()) + Math.abs(this.y - c.getY());
+	}
+
+	/**
+	 * getter x
+	 * 
+	 * @return x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * set coordonnee x
+	 * 
+	 * @param x
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * getter y
+	 * 
+	 * @return y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * set coordonnee y
+	 * 
+	 * @param y
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 
 }

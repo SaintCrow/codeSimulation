6b8a471b500aa5a57bddafd07777c83a1ea82ac3
SaintCrow <valehue@hotmail.fr>
2018-05-14 13:52:19 +0200

    correction

--- src/pkg_fourmi/Fourmi.java
+++ src/pkg_fourmi/Fourmi.java
@@ -49,16 +49,16 @@ public abstract class Fourmi extends Insecte {
 		int y = coordFourmi.getY();
 		for (int i = x - 4; i <= x + 4; i++) {
 			for (int j = y - 4; j <= y + 4; j++) {
-				if (new Coordonnee(i, j).distance(coordFourmi) <= 4) {
+				if (new Coordonnee(i, j).euclidienne(coordFourmi) <= 4) {
 					Simulation.getGrille()[x][y].addPheroDanger(5);
 				}
-				if (new Coordonnee(i, j).distance(coordFourmi) <= 3) {
+				if (new Coordonnee(i, j).euclidienne(coordFourmi) <= 3) {
 					Simulation.getGrille()[x][y].addPheroDanger(5);
 				}
-				if (new Coordonnee(i, j).distance(coordFourmi) <= 2) {
+				if (new Coordonnee(i, j).euclidienne(coordFourmi) <= 2) {
 					Simulation.getGrille()[x][y].addPheroDanger(5);
 				}
-				if (new Coordonnee(i, j).distance(coordFourmi) <= 1) {
+				if (new Coordonnee(i, j).euclidienne(coordFourmi) <= 1) {
 					Simulation.getGrille()[x][y].addPheroDanger(5);
 				}
 			}
@@ -107,6 +107,49 @@ public abstract class Fourmi extends Insecte {
 			return coordPlusProche;
 		}
 
+	}
+	
+	public Coordonnee recherchePheromoneNourriture(){
+		Coordonnee coordFourmi = this.getPosition();
+		int x = coordFourmi.getX();
+		int y = coordFourmi.getY();
+		ArrayList<Coordonnee> listPheromone = new ArrayList<Coordonnee>();
+		for (int i = x - getChampvision(); i <= x + this.getChampvision(); i++) {
+			for (int j = y - getChampvision(); j <= y + this.getChampvision(); j++) {
+				if ((new Coordonnee(i, j).estCorrecte()) && (Simulation.getGrille()[i][j].getPheroDanger() != 0)) {
+					listPheromone.add(Simulation.getGrille()[i][j].getPosition());
+				}
+			}
+		}
+		if (listPheromone.size() == 0) {
+			return null;
+		} else if (listPheromone.size() == 1) {
+			return listPheromone.get(0);
+		} else {
+			int xPhero = listPheromone.get(0).getX();
+			int yPhero = listPheromone.get(0).getY();
+			int intenMax = Simulation.getGrille()[xPhero][yPhero].getPheroDanger();
+			ArrayList<Coordonnee> listPheromonePlusForte = new ArrayList<Coordonnee>();
+			for (int i = 0; i < listPheromone.size(); i++) {
+				xPhero = listPheromone.get(i).getX();
+				yPhero = listPheromone.get(i).getY();
+				if (Simulation.getGrille()[xPhero][yPhero].getPheroDanger() > intenMax) {
+					intenMax = Simulation.getGrille()[xPhero][yPhero].getPheroDanger();
+					listPheromonePlusForte.clear();
+					listPheromonePlusForte.add(listPheromone.get(i));
+				}
+				if (Simulation.getGrille()[xPhero][yPhero].getPheroDanger() == intenMax) {
+					listPheromonePlusForte.add(listPheromone.get(i));
+				}
+			}
+			Coordonnee coordPlusProche = listPheromonePlusForte.get(0);
+			for (int i = 1; i < listPheromonePlusForte.size(); i++) {
+				if (coordFourmi.distance(listPheromonePlusForte.get(i)) < coordFourmi.distance(coordPlusProche)) {
+					coordPlusProche = listPheromonePlusForte.get(i);
+				}
+			}
+			return coordPlusProche;
+		}
 	}
 
 	@Override
